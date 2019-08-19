1659673f51a88315bf501c13617a5d166ca4f4cc
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-19 08:41:09 +0100

    Add navigation from SleepTracker screen to SleepQuality

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,6 +17,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
+import android.text.Spanned
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -41,12 +42,21 @@ class SleepTrackerViewModel(
 
     private val nights = database.getAllNights()
 
-    val nightString = Transformations.map(nights){ nights ->
+    val nightString: LiveData<Spanned> = Transformations.map(nights){ nights ->
         formatNights(nights, application.resources)
     }
 
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+
     init {
         initializeTonight()
+    }
+
+
+    fun doneNavigating(){
+        _navigateToSleepQuality.value = null
     }
 
     private fun initializeTonight() {
@@ -86,6 +96,8 @@ class SleepTrackerViewModel(
             oldNight.endTimeMilli = System.currentTimeMillis()
 
             update(oldNight)
+
+            _navigateToSleepQuality.value = oldNight
         }
     }
 
